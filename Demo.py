582495f40a6b1c32769fa852363146c41ad78510
samarthbhatsrs <samarthbhatsirsi@gmail.com>
2024-12-21 15:38:41 +0530

    Code Changed

--- Demo.py
+++ Demo.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-a = 'Sam'
-b = 'Prajwal'
-c = 'Shambhu'
-d = a+b+c+d 
-=======
 s1 = 'Samarth'
 s2 = 'Goutham'
 s3 = 'Akshay'
 s4 = s1+s2+s3
->>>>>>> 7da9cb5e6cb68522698f92e2603a445708626293
+
